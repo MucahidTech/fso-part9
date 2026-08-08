31238da01cceb9da738033953171db4f7905cd5f
MucahidTech <mucahid.tech@gmail.com>
2026-08-08 16:18:27 +0300

    feat(healthapp): add BMI endpoint with query params and module import - ex 9.5

--- healthapp/bmiCalculator.ts
+++ healthapp/bmiCalculator.ts
@@ -1,4 +1,4 @@
-const calculateBmi = (height: number, weight: number): string => {
+export const calculateBmi = (height: number, weight: number): string => {
   const heightInMeters = height > 2.5 ? height / 100 : height;
   const bmi: number = weight / heightInMeters ** 2;
 
@@ -10,17 +10,14 @@ const calculateBmi = (height: number, weight: number): string => {
 
 const args = process.argv.slice(2);
 
-if (args.length !== 2) {
-  console.error("Usage: npm run calculateBmi <height_in_cm> <weight_in_kg>");
-  process.exit(1);
+if (args.length === 2) {
+  const height = Number(args[0]);
+  const weight = Number(args[1]);
+
+  if (isNaN(height) || isNaN(weight) || height <= 0 || weight <= 0) {
+    console.error("Height and weight must be positive numbers.");
+    process.exit(1);
+  }
+
+  console.log(calculateBmi(height, weight));
 }
-
-const height = Number(args[0]);
-const weight = Number(args[1]);
-
-if (isNaN(height) || isNaN(weight) || height <= 0 || weight <= 0) {
-  console.error("Height and weight must be positive numbers.");
-  process.exit(1);
-}
-
-console.log(calculateBmi(height, weight));
